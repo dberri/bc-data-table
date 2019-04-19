5b5e93890733d4ce85d8930d389c76525e0ef52f
David Berri <dwbwill@gmail.com>
2019-04-19 09:08:01 -0300

    DataTable component

---
+++ src/components/DataTable.vue
@@ -0,0 +1,20 @@
+<template></template>
+
+<script>
+export default {
+  name: "DataTable",
+
+  computed: {
+    payments() {
+      return this.$store.state.payments;
+    }
+  },
+
+  mounted() {
+    this.$store.dispatch("fetchPayments");
+  }
+};
+</script>
+
+<style scoped lang="scss">
+</style>
