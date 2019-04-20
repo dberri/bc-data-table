b41ca37889764280f28a1bfad6ac49939211f28f
David Berri <dwbwill@gmail.com>
2019-04-20 15:50:39 -0300

    App available to cypress

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-new Vue({
+const app = new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
+
+if (window.Cypress) {
+  // only available during E2E tests
+  window.app = app;
+}
