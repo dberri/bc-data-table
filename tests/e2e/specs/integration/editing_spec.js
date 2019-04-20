b42bd6a5a951bac4a2b1c1b4691d74d685b28f9e
David Berri <dwbwill@gmail.com>
2019-04-20 15:49:27 -0300

    More test cases

--- tests/e2e/specs/integration/editing_spec.js
+++ tests/e2e/specs/integration/editing_spec.js
@@ -1,1 +1,31 @@
-describe('Tests editability', () => {});
+const sentence = ' quam felis, ultricies nec';
+const description = 'et magnis dis parturient montes, nascetur ridiculus mus. Donec';
+
+describe('Tests editability', () => {
+  it('Shows one editing box when clicks edit button', () => {
+    cy.visit('/');
+
+    cy.get('tbody tr:first-child td:nth-child(2)')
+      .trigger('mouseover')
+      .find('button.edit-btn')
+      .click();
+
+    cy.get('.edit-box.active').should('have.length', 1);
+
+    cy.get('tbody tr:first-child td:nth-child(2)')
+      .find('.edit-box.active')
+      .should('be.visible');
+  });
+
+  it('Edits the description of the first payment on the list', () => {
+    cy.get('tbody tr:first-child td:nth-child(2)').contains(description);
+
+    cy.get('.edit-box.active')
+      .find('input')
+      .should('have.value', description);
+
+    cy.get('.edit-box.active input').type(sentence);
+    cy.get('.edit-box.active button[type=submit]').click();
+    cy.contains(`${description}${sentence}`, { timeout: 10000 });
+  });
+});
--- tests/e2e/specs/integration/sorting_spec.js
+++ tests/e2e/specs/integration/sorting_spec.js
@@ -1,36 +1,55 @@
+const getStore = () => cy.window().its('app.$store');
+
 describe('Tests sortability', () => {
-  it('Loads payments', () => {
+  it('Starts empty', () => {
     cy.visit('/');
+    getStore()
+      .its('state')
+      .should('deep.equal', {
+        payments: [],
+      });
+  });
+
+  it('Loads payments', () => {
+    getStore()
+      .its('state.payments')
+      .should('have.length', 100);
     cy.get('tbody tr').should('have.length', 100);
   });
 
   it('Sorts table by Name in ascending order when user clicks on the Name column header', () => {
     cy.get('thead th:first-child').click();
     cy.get('tbody tr:first-child td:first-child').contains('Aimee Davidson');
+    cy.get('tbody tr:last-child td:first-child').contains('Yael Horn');
   });
 
   it('Sorts table by Name in descending order when user clicks on the Name column header again', () => {
     cy.get('thead th:first-child').click();
     cy.get('tbody tr:first-child td:first-child').contains('Yael Horn');
+    cy.get('tbody tr:last-child td:first-child').contains('Aimee Davidson');
   });
 
   it('Sorts table by Date in ascending order when user clicks on the Date column header', () => {
     cy.get('thead th:nth-child(3)').click();
     cy.get('tbody tr:first-child td:nth-child(3)').contains('2017-05-04T10:11:50-07:00');
+    cy.get('tbody tr:last-child td:nth-child(3)').contains('2019-10-28T04:57:29-07:00');
   });
 
   it('Sorts table by Date in descending order when user clicks on the Date column header again', () => {
     cy.get('thead th:nth-child(3)').click();
     cy.get('tbody tr:first-child td:nth-child(3)').contains('2019-10-28T04:57:29-07:00');
+    cy.get('tbody tr:last-child td:nth-child(3)').contains('2017-05-04T10:11:50-07:00');
   });
 
   it('Sorts table by Amount in ascending order when user clicks on the Amount column header', () => {
     cy.get('thead th:nth-child(4)').click();
     cy.get('tbody tr:first-child td:nth-child(4)').contains(-792.3);
+    cy.get('tbody tr:last-child td:nth-child(4)').contains(967.89);
   });
 
   it('Sorts table by Amount in descending order when user clicks on the Amount column header again', () => {
     cy.get('thead th:nth-child(4)').click();
     cy.get('tbody tr:first-child td:nth-child(4)').contains(967.89);
+    cy.get('tbody tr:last-child td:nth-child(4)').contains(-792.3);
   });
 });
