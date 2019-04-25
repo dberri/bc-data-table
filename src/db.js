f7205c9d5ec19629d03f9d4535363c46ce2e84ee
David Berri <dwbwill@gmail.com>
2019-04-24 23:00:12 -0300

    Removed firebase credentials 🤦 (also rotated it in the back end)

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: 'AIzaSyC01LuRUqfWK8tBvG8JeXlMATpm3IiPIx0',
-  authDomain: 'bc-data-table.firebaseapp.com',
-  databaseURL: 'https://bc-data-table.firebaseio.com',
-  projectId: 'bc-data-table',
-  storageBucket: 'bc-data-table.appspot.com',
-  messagingSenderId: '125946695204',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
 };
 
 export default firebase.initializeApp(config).firestore();
